9c3c84f0f91bacc452e525bf7b5c9106c6200507
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-03 11:26:43 -0500

    Begin code for establishing database connection using JDBC and DriverManager

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -1,5 +1,25 @@
 package dbUtil;
 
-public class dbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class dbConnection {
+	//for MYSQL (not needed but just for show)
+	private static final String USERNAME = "dbuser";
+	private static final String PASSWORD = "dbpassword";
+	private static final String CONN = "jdbc:mysql://localhost/login";
+	//for SQLite we do not need the above
+	private static final String SQCONN = "jdbc:sqlite:schoolsystem.sqlite";
+	
+	public static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("org.sqlite.JDBC");
+			return DriverManager.getConnection(SQCONN);
+		}catch(ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
 }
