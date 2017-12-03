37fb6d6376a811555cd9f55f91a69579698cabf4
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-03 12:44:22 -0500

    Connect LoginController to Login view and database

--- src/loginApp/LoginController.java
+++ src/loginApp/LoginController.java
@@ -1,5 +1,36 @@
 package loginApp;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController implements Initializable {
+	LoginModel loginModel = new LoginModel();
+	
+	@FXML
+	private Label dbStatus;
+	@FXML
+	private TextField username;
+	@FXML
+	private PasswordField password;
+	@FXML
+	private ComboBox<option> combobox;
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		if(this.loginModel.isDatabaseConnected())
+			this.dbStatus.setText("Connected to Database");
+		else
+			this.dbStatus.setText("Not Connected To Database");
+		this.combobox.setItems(FXCollections.observableArrayList(option.values()));
+	}
 
 }
